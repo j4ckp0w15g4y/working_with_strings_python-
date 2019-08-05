dcfa7fd5d59552d053cf74cc67186e8e6911219d
Jack Gay <jackpowis@gmail.com>
2019-08-05 17:28:58 +0100

    commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# print("Giraffe\nAcademy")
+#
+# print("Giraffe\"Academy")
+#
+# print("Giraffe\Academy")
+#
+
+
+phrase = "Giraffe Academy"
+# print(phrase + " is cool")
+
+# print(phrase.lower())
+
+# print(phrase.upper())
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
+
