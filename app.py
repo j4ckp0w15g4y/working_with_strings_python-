d7ee81bf6eebe2112c3ea02a18e6365b2dfc6fd4
Jack Gay <jackpowis@gmail.com>
2019-08-05 17:51:06 +0100

    end of number tutorial

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from math import *
+# above is importing external math functions
+
+
+
 # STRINGS
 
 # # print("Giraffe\nAcademy")
@@ -47,6 +52,15 @@
 my_num = -5
 
 print(abs(my_num))
+print(pow(3, 2))
+print(pow(4, 6))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.2))
+print(round(3.7))
 
+# LOOK AT LINE ONE: IMPORTING EXTERNAL MATH FUNCTIONS
 
-
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
