53686222febcc31abd8b14e34f84f31c0e49c750
Jack Gay <jackpowis@gmail.com>
2019-08-05 17:35:53 +0100

    numbers  commit

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-# print("Giraffe\nAcademy")
+# STRINGS
+
+# # print("Giraffe\nAcademy")
+# #
+# # print("Giraffe\"Academy")
+# #
+# # print("Giraffe\Academy")
+# #
 #
-# print("Giraffe\"Academy")
 #
-# print("Giraffe\Academy")
+# phrase = "Giraffe Academy"
+# # print(phrase + " is cool")
+#
+# # print(phrase.lower())
+#
+# # print(phrase.upper())
+#
+# print(phrase.upper().isupper())
+#
+# print(len(phrase))
+#
+# print(phrase[3])
+#
+# print(phrase.index("G"))
+# print(phrase.index("a"))
+# # print(phrase.index("z"))
+#
+# print(phrase.replace("Giraffe", "Elephant"))
 #
 
-
-phrase = "Giraffe Academy"
-# print(phrase + " is cool")
-
-# print(phrase.lower())
-
-# print(phrase.upper())
-
-print(phrase.upper().isupper())
-
-print(len(phrase))
+# NUMBERS
 
