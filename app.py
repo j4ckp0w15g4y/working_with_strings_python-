3420fa7c33d5a0b878506392c11b961133b955a2
Jack Gay <jackpowis@gmail.com>
2019-08-05 17:45:14 +0100

    "looking at number methods with giraffe academy"

--- app.py
+++ app.py
@@ -30,3 +30,23 @@
 
 # NUMBERS
 
+# my_num = 5
+#
+# print(-2.0987)
+#
+# print(3 / 4.5)
+#
+# print(3 * (4 + 5))
+#
+# print(10 % 3)
+#
+# print(my_num)
+#
+# print(str(my_num) + " my favourite number")
+
+my_num = -5
+
+print(abs(my_num))
+
+
+
